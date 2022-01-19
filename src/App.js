fcc5c94624eb60378a7e5a00d22819263e56c75f
Paula Illanes <peudemaret@Peus-MacBook-Air.local>
2022-01-18 23:05:36 -0300

    🎉 First commit! Starting recipe repository

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <>
+            <nav></nav>
+            <main role="main">
+                <p>Hola</p>
+                <strong>Hola</strong>
+            </main>
+            <footer></footer>
+        </>
+    );
+};
 
 export default App;
