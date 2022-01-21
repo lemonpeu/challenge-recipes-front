cdcaa4a2e2374d2a693513fa44739ae6f587e76c
Peu <48685783+lemonpeu@users.noreply.github.com>
2022-01-21 07:41:29 -0600

    Update/readme (#9)
    * WIP. Adding rating functionality
    
    * Uploading readme
    
    Co-authored-by: Paula Illanes <peudemaret@Peus-MacBook-Air.local>

--- src/common/RecipeArticle.js
+++ src/common/RecipeArticle.js
@@ -4,6 +4,7 @@ import Image from './Image';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '../utils/hooks/useQuery';
 import Form from './Form';
+import Rating from '../sections/article/Rating';
 
 const RecipeArticle = () => {
     let { id } = useParams();
@@ -25,7 +26,6 @@ const RecipeArticle = () => {
     const multiplyQuantity = (event) => {
         event.preventDefault();
         let value = parseInt(event.target.quantity.value);
-        console.log('Value', value);
         setUserQuantity(value);
     };
 
@@ -36,6 +36,10 @@ const RecipeArticle = () => {
                     <Image src={recipeObj.urlAnimeImage} alt={recipeObj.name} />
                     <h2>{recipeObj.name}</h2>
                     <p>Rating: {recipeObj.rating / recipeObj.people} ⭐</p>
+                    <div>
+                        <p>Send your rating for this recipe:</p>
+                        <Rating id={recipeObj.id} />
+                    </div>
                     <Form
                         defaultValue={recipeObj.quantity}
                         btnName="Send"
@@ -46,15 +50,17 @@ const RecipeArticle = () => {
                     <div className="ingredients">
                         <p>Ingredients:</p>
                         <ul>
-                            {recipeObj.Ingredients.map((ingredient) => (
-                                <li>- {`${setDefaultQuantity(ingredient)} ${ingredient.name}`}</li>
+                            {recipeObj.Ingredients.map((ingredient, index) => (
+                                <li key={index}>
+                                    - {`${setDefaultQuantity(ingredient)} ${ingredient.name}`}
+                                </li>
                             ))}
                         </ul>
                     </div>
                     <div className="instructions">
                         <p>Instructions:</p>
-                        {recipeObj.steps.map((step) => (
-                            <p>{step}</p>
+                        {recipeObj.steps.map((step, index) => (
+                            <p key={index}>{step}</p>
                         ))}
                     </div>
                 </>
